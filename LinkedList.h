d58369ab25cc7b4e1814107947d3f74668993fd7
Trace Boggs <155930465+TBoggs05@users.noreply.github.com>
2024-04-09 17:47:10 -0500

    Update LinkedList.h

--- LinkedList.h
+++ LinkedList.h
@@ -1,33 +1,88 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
+#include<iostream>
 #include<string>
 struct Node {
+	//passenger data for each passenger Node
 	std::string firstName;
 	std::string lastName;
 	std::string address;
 	int passengerID;
-	int reservationNum;
-	int phoneNum;
+	long reservationNum;
+	std::string phoneNum;
 	int seatNum;
 	std::string menuPref;
 	Node* next;
+	//constructor that sets next to NULL by default, and everything else to blank or -1.
+	Node() {
+		next = NULL;
+		firstName = lastName = address = phoneNum = menuPref = " ";
+		reservationNum = -1;
+		passengerID = seatNum = -1;
+	}
+	//public functions to streamline Linked List operations.
+	/*
+	* simply sets data as passed.
+	@RETURNS VOID
+	*/
+	void setData(std::string first, std::string last, std::string addr, int passID, long resNum, std::string pNum, int sNum, std::string mPref, Node* n) {
+		firstName = first;
+		lastName = last;
+		address = addr;
+		passengerID = passID;
+		reservationNum = resNum;
+		phoneNum = pNum;
+		seatNum = sNum;
+		menuPref = mPref;
+		next = n;
+	}
+	/*
+	-checks for alphabetical order.
+	-uses built in compare function, which uses ASCII Values.
+	-returns 1 if objects lastName is EARLIER in the alphabet than the compared Node's lastName.
+	-returns 0 otherwise
+	-If both lastnames are equal, we compare firstNames.
+	@RETURNS BOOL
+	*/
+	bool compareLastName(Node* other) {
+		//if lastNames are equal, check firstNames.
+		if (this->lastName == other->lastName) {
+			//other firstName comes earlier in the alphabet than this firstName
+			if (this->firstName > other->firstName) {
+				return 1;
+			}
+			//vice versa
+			else {
+				return 0;
+			}
+		}
+		//other lastName comes earlier in the alphabet than this lastName
+		if (this->lastName > other->lastName) {
+			return 1;
+		}
+		//vice versa
+		else {
+			return 0;
+		}
+	}
 };
 
 class LinkedList
 {
 private:
-	int count;
-	Node* head;
+	int count;//# of nodes
+	Node* head;//first node. (linear linked list)
 public:
-	LinkedList();
-	~LinkedList();
-	LinkedList(Linked_List*);
-	void LinkedList_copy(Node*);
-	bool insert_node(Node*);
-	bool delete_node(Node*);
-	void print_list();
-	int list_size();
-	int search_node(Node* n);
+	LinkedList();//initializes linked list
+	~LinkedList();//frees memory used by linkedList
+	LinkedList(LinkedList*);//initialize a linked list with another linkedList.
+	void LinkedList_copy(Node*);//copy a linked list to our linked list, overwriting our data.
+	bool insert_node(Node*);//inserts node alphabetically using Node's compareLastName() function
+	bool delete_node(long);//deletes a node after finding it based on ID
+	void print_list();//prints each node's data
+	int list_size();//returns count
+	int search_node(long );//searches for a node based on ID
+	Node* getHead() { return head; }//inline function to return head(only useful for copy function)
 };
 
 #endif
